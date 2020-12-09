439f3e93a94694b3199411e05d16e57ae52c420f
Judy Huang <judyhuang0621@hotmail.com.tw>
2020-12-10 02:08:53 +0800

    input

--- 2020CG_Lab2_109522104/2020CG_Lab2_109522104/2020CG_Lab2_109522104.cpp
+++ 2020CG_Lab2_109522104/2020CG_Lab2_109522104/2020CG_Lab2_109522104.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<gl/GLUT.H>
 #include<string>
+#include <fstream>
 
 void Initial(void)
 {
@@ -23,81 +24,86 @@ void drawDot(int x, int y) {
 	cout << "dot on " << x << ", " << y << endl;
 }
 
+ifstream input;
+
 void myDisplay(void) { 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glFlush();
 
 	string c = "";
-	cin >> c;
-	if (c == "#") {
-		cout << c << " ";
-		getline(cin, c);
-		cout << c << endl;
-		//break;
+	while (input >> c) {
+		if (c == "#") {
+			cout << c << " ";
+			getline(cin, c);
+			cout << c << endl;
+			//break;
+		}
+		else if (c == "reset") {
+			cout << c << endl;
+			//break;
+		}
+		else if (c == "scale") {
+			float a, b = 0;
+			cin >> a >> b;
+			drawDot((int)floor(a), (int)floor(b));
+			cout << c << " " << a << " " << b << endl;
+			//break;
+		}
+		else if (c == "rotate") {
+			float theta = 0;
+			cin >> theta;
+			cout << c << " " << theta << endl;
+			//break;
+		}
+		else if (c == "translate") {
+			float x, y = 0;
+			cin >> x >> y;
+			cout << c << " " << x << " " << y << endl;
+			//break;
+		}
+		else if (c == "square") {
+			cout << c << endl;
+			//break;
+		}
+		else if (c == "triangle") {
+			cout << c << endl;
+			//break;
+		}
+		else if (c == "view") {
+			float wxl, wxr, wyb, wyt, vxl, vxr, vyb, vyt = 0;
+			cin >> wxl >> wxr >> wyb >> wyt >> vxl >> vxr >> vyb >> vyt;
+			cout << c << endl;
+			system("pause");
+			//break;
+		}
+		else if (c == "clearData") {
+			cout << c << endl;
+			//break;
+		}
+		else if (c == "clearScreen") {
+			cout << c << endl;
+			//break;
+		}
+		else if (c == "end") {
+			cout << c << endl;
+			system("pause");
+			glutDestroyWindow(glutCreateWindow("2020CG_Lab2_109522104"));
+			exit(0);
+			//break;
+		}
+		else {
+			cout << "unexpected token " << c << endl;
+			//break;
+		}
 	}
-	else if (c == "reset") {
-		cout << c << endl;
-		//break;
-	}
-	else if (c == "scale") {
-		float a, b = 0;
-		cin >> a >> b;
-		drawDot((int)floor(a), (int)floor(b));
-		cout << c << " " << a << " " << b << endl;
-		//break;
-	}
-	else if (c == "rotate") {
-		float theta = 0;
-		cin >> theta;
-		cout << c << " " << theta << endl;
-		//break;
-	}
-	else if (c == "translate") {
-		float x, y = 0;
-		cin >> x >> y;
-		cout << c << " " << x << " " << y << endl;
-		//break;
-	}
-	else if (c == "square") {
-		cout << c << endl;
-		//break;
-	}
-	else if (c == "triangle") {
-		cout << c << endl;
-		//break;
-	}
-	else if (c == "view") {
-		float wxl, wxr, wyb, wyt, vxl, vxr, vyb, vyt = 0;
-		cin >> wxl >> wxr >> wyb >> wyt >> vxl >> vxr >> vyb >> vyt;
-		cout << c << endl;
-		system("pause");
-		//break;
-	}
-	else if (c == "clearData") {
-		cout << c << endl;
-		//break;
-	}
-	else if (c == "clearScreen") {
-		cout << c << endl;
-		//break;
-	}
-	else if (c == "end") {
-		cout << c << endl;
-		system("pause");
-		glutDestroyWindow(glutCreateWindow("2020CG_Lab2_109522104"));
-		exit(0);
-		//break;
-	}
-	else {
-		cout << "unexpected token " << c << endl;
-		//break;
-	}
+	
 	// system("pause");	
 }
 
 bool lflag = false, pflag = false, oflag = false;
 int X0 = 0, Y0 = 0, X1 = 0, Y1 = 0, X2 = 0, Y2 = 0;
+
 
 void drawLine(float x1, float y1, float x2, float y2) {
 	cout << "you made it to drawLine.\n";
@@ -154,10 +160,22 @@ int main(int argc, char * argv[])
 	glutCreateWindow("2020CG_Lab2_109522104");
 	// Initial();
 
-	glutDisplayFunc(myDisplay);
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glutMainLoop();
+	if (argc < 2) {
+		cout << "Please use command line to enter input file name." << endl;
+		return 0;
+	}
+
+	input.open(argv[1]);
+	if (input.is_open())
+	{
+		glutDisplayFunc(myDisplay);
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glutMainLoop();
+		input.close();
+	}
+	else cout << "Unable to open file";
+
 
 	return 0;
 }
